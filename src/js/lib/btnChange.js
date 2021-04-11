bef19f37e92c7e982477088efe294ba13d51c8f6
Amet <azizov.amet@gmai.com>
2021-04-12 01:59:47 +0300

    asdsadasdsad

--- src/js/lib/btnChange.js
+++ src/js/lib/btnChange.js
@@ -1,10 +1,14 @@
-// const mainElOffetTop = document.querySelector('main').offsetTop;
-// console.log(mainElOffetTop);
-// $(window).on("scroll", function() {
-// 	if($(window).scrollTop() > mainElOffetTop) {
-// 		$(".nvm").addClass("btn-color");
-// 	} else {
-// 		//remove the background property so it comes transparent again (defined in your css)
-// 		$(".nvm").removeClass("btn-color");
-// 	}
-// });
+const nvmBtn = $('.nvm');
+console.log(nvmBtn);
+if(nvmBtn.length > 0) {
+    const mainElOffetTop = document.querySelector('main').offsetTop;
+    console.log(mainElOffetTop);
+    $(window).on("scroll", function() {
+        if($(window).scrollTop() > mainElOffetTop) {
+            $(".nvm").addClass("btn-color");
+        } else {
+            //remove the background property so it comes transparent again (defined in your css)
+            $(".nvm").removeClass("btn-color");
+        }
+    });
+}
